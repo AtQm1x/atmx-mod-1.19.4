78bfc43aa81472cb09966a4de13f435ded6d86b3
AtQm1x <denplay5200@gmail.com>
2023-11-10 09:17:41 +0200

    Initial commit

---
+++ src/main/java/com/atmx/AtomixMod/atmxModDataGenerator.java
@@ -0,0 +1,11 @@
+package com.atmx.AtomixMod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class atmxModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
